e1dae4c63cc37c3fdd1bfd7b1b753e8c4b302f6f
suman11111 <36926166+suman11111@users.noreply.github.com>
2019-09-14 16:53:09 +0530

    Add files via upload

---
+++ skip and delete linked list.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+class Node
+{	public:
+	int data;
+	Node *next;
+};
+
+void push(Node** head_ref,int new_data)   //HEADREF IS POINTER NOT TO NODE BUT POINTER TO POINTER WHICH I POINTING TO NODE;
+{
+	Node* new_node=new Node();
+	new_node->data=new_data;
+	new_node->next=(*head_ref); 
+	(*head_ref)=new_node;/*  move the head to point to the new node */
+}
+
+void skippeddeletenode(Node** head_ref,int m,int n)
+{
+	if(*head_ref==NULL)
+	cout<<"not possible";
+	
+	Node* temp= *head_ref;
+	Node* prev;
+	
+	while(m-1 && temp!=NULL)
+	{
+		temp=temp->next;
+		m--;
+	}
+	
+	
+	while(n && temp!=NULL)
+	{
+		prev= temp;
+		if(temp->next)   //jb temp->next null nhi hoga tb andr jayega
+		{
+			temp=temp->next;
+			prev->next=temp->next;
+			free(temp);
+			temp=prev;
+			n--;
+		}
+		else
+		{
+			break;
+		}
+	}
+	
+}
+
+
+
+  
+void printList(Node *node)
+{
+	while(node!=NULL)
+	{
+		cout<<" "<<node->data;
+		node=node->next;
+	}
+	
+}
+
+
+
+int main()
+{
+	Node* head=NULL; //head is an node pointer   but pointing to null,naya naam diye hai head 
+	push(&head,6);
+	push(&head,4);
+	push(&head,2);
+	push(&head,1);
+	cout<<"After pushing data";
+	printList(head);
+	int m=3,n=3;
+	
+	skippeddeletenode(&head,m,n);
+	
+	cout<<"\n";
+	printList(head);
+	
+	
+	return 0;
+	
+	
+}
